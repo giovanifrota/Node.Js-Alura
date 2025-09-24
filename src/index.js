b373a8e70b84d6601e4b13e3e2b3df281f5acf56
Giovani Frota <giovanifrota45@gmail.com>
2025-09-24 10:38:56 -0300

    Tratamento de erros

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const fs = require("fs"); //FS é File System faz com que vc interaja com sistema de arquivos do computador (É UMA BIBLIOTECA DO NODE)
+const trataErros = require('./erros/funcoesErros');
 
 const caminhoArquivo = process.argv; //Esse comando pega os valores que sao passados no terminal e coloca eles em um array
 const link = caminhoArquivo[2];
 
 fs.readFile(link, "utf-8", (err, texto) => {
-    if(err){
-        console.log('Qual o erro? ', err.code);
-        return
+    try{
+        if(err) throw err;
+        contaPalavras(texto);
+    } catch(err){
+        trataErros(err);
     }
-  contaPalavras(texto);
 });
 
 function contaPalavras(texto) {
@@ -20,7 +22,7 @@ function contaPalavras(texto) {
   console.log(contagem);
 }
 
-function extraiParagrafos() {
+function extraiParagrafos(texto) {
   return texto.toLowerCase().split("\n");
 }
 
